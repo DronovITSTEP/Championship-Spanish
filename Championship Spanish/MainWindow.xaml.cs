6c724b81d04995d074a37a1269133fe07ad5a5d9
daniil <skrimexgame12508@mail.ru>
2024-12-23 20:55:27 +0300

    Добавьте файлы проекта.

---
+++ Championship Spanish/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Championship_Spanish
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /* select * from Teams where name= @name; */
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            using (ChampionshipEntities db = new ChampionshipEntities())
+            {
+                
+               dataGrid.ItemsSource = db.Teams
+                    .Where(name => name.Name == textBox.Text);
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            using (ChampionshipEntities db = new ChampionshipEntities())
+            {
+
+                dataGrid.ItemsSource = db.Teams
+                     .Where(name => name.City == textBox.Text);
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            using (ChampionshipEntities db = new ChampionshipEntities())
+            {
+
+                dataGrid.ItemsSource = db.Teams
+                     .Where(name => name.Name == textBox.Text &&
+                            name.City == textBox.Text);
+            }
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            /*отобразить команды с самым большим количеством побед*/
+            using (ChampionshipEntities db = new ChampionshipEntities())
+            {
+               
+            }
+        }
+    }
+}
---
+++ Championship Spanish/create_tables.sql
@@ -0,0 +1,23 @@
+﻿create table Teams (
+id int primary key,
+Name nvarchar(50)
+);
+
+create table Players (
+id int primary key,
+FIO nvarchar(100),
+Country nvarchar(50),
+NumberPlayer int,
+Position nvarchar(25),
+Team int foreign key references Teams(id)
+);
+create table TeamFights (
+id int primary key,
+Team1 int foreign key references Teams(id),
+Team2 int foreign key references Teams(id),
+CountGoal1 int,
+CountGoal2 int,
+PlayerGoal int foreign key references Players(id),
+MatchDate date
+);
+
